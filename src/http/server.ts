bb59409ced3dad285d51251d642b0b7b51dc6745
Alessandro1918 <alessandro1918@hotmail.com>
2024-02-06 00:28:11 -0300

    feat: new route: createNewPoll

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,30 @@
 import fastify from "fastify"
+import { PrismaClient } from "@prisma/client"
+import { z } from "zod"
+
+const PORT = 4000
 
 const app = fastify()
 
-const PORT = 4000
+const prisma = new PrismaClient()
+
+app.post("/polls", async (request, reply) => {
+  const reqBody = z.object({
+    title: z.string(),
+  })
+
+  const { title } = reqBody.parse(request.body)
+
+  const poll = await prisma.poll.create({
+    data: {
+      title,
+    }
+  })
+
+  return reply
+    .status(201)
+    .send({ pollId: poll.id })
+})
 
 app
   .listen({port: PORT})
